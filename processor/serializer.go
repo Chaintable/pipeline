a6edecbbc1db1212fd8db86071c11d772268811b
Zhang Guyu <zhangguyu6@gmail.com>
2024-10-27 03:10:29 +0000

    fix use blockhash as prefix

--- processor/serializer.go
+++ processor/serializer.go
@@ -12,80 +12,80 @@ type DataFile struct {
 	Data  []byte
 }
 
-// s3Key: block/<blockHash>
+// s3Key: <blockHash>/block
 func SerializeBlock(block *types.Block) (*DataFile, error) {
 	data, err := util.EncodeToJsonGzip(block)
 	if err != nil {
 		return nil, err
 	}
-	s3Key := fmt.Sprintf("block/%s", block.Hash.Hex())
+	s3Key := fmt.Sprintf("%s/block", block.Hash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
 	}, nil
 }
 
-// s3Key: transaction/<blockHash>/<transactionHash>
+// s3Key: <blockHash>/transaction/<transactionHash>
 func SerializeTransaction(transaction *types.Transaction) (*DataFile, error) {
 	data, err := util.EncodeToJsonGzip(transaction)
 	if err != nil {
 		return nil, err
 	}
-	s3Key := fmt.Sprintf("transaction/%s/%s", transaction.BlockHash.Hex(), transaction.Hash.Hex())
+	s3Key := fmt.Sprintf("%s/transaction/%s", transaction.BlockHash.Hex(), transaction.Hash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
 	}, nil
 }
 
-// s3Key: receipt/<blockHash>/<transactionHash>
+// s3Key: <blockHash>/receipt/<transactionHash>
 func SerializeReceipt(receipt *types.Receipt) (*DataFile, error) {
 	data, err := util.EncodeToJsonGzip(receipt)
 	if err != nil {
 		return nil, err
 	}
-	s3Key := fmt.Sprintf("receipt/%s/%s", receipt.BlockHash.Hex(), receipt.TransactionHash.Hex())
+	s3Key := fmt.Sprintf("%s/receipt/%s", receipt.BlockHash.Hex(), receipt.TransactionHash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
 	}, nil
 }
 
-// s3Key: trace/<blockHash>/<transactionHash>/<traceHash>
+// s3Key: <blockHash>/trace/<transactionHash>/<traceHash>
 func SerializeTrace(trace types.CallFrame) (*DataFile, error) {
 	data, err := util.EncodeToJsonGzip(trace)
 	if err != nil {
 		return nil, err
 	}
 	traceHash := trace.Hash()
-	s3Key := fmt.Sprintf("trace/%s/%s/%s", trace.BlockHash.Hex(), trace.TransactionHash.Hex(), traceHash.Hex())
+	s3Key := fmt.Sprintf("%s/trace/%s/%s", trace.BlockHash.Hex(), trace.TransactionHash.Hex(), traceHash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
 	}, nil
 }
 
-// s3Key: event/<blockHash>/<transactionHash>/<eventHash>
+// s3Key: <blockHash>/event/<transactionHash>/<eventHash>
 func SerializeEvent(event *types.Event) (*DataFile, error) {
 	data, err := util.EncodeToJsonGzip(event)
 	if err != nil {
 		return nil, err
 	}
 	eventHash := event.Hash()
-	s3Key := fmt.Sprintf("event/%s/%s/%s", event.BlockHash.Hex(), event.TxHash.Hex(), eventHash.Hex())
+	s3Key := fmt.Sprintf("%s/event/%s/%s", event.BlockHash.Hex(), event.TxHash.Hex(), eventHash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
 	}, nil
 }
 
-// s3Key: stateDiff/<blockHash>
+// s3Key: <blockHash>/stateDiff
 func SerializeStateDiff(stateDiff *types.BlockStorageDiff) (*DataFile, error) {
 	data, err := util.EncodeToRlp(stateDiff)
 	if err != nil {
 		return nil, err
 	}
-	s3Key := fmt.Sprintf("stateDiff/%s", stateDiff.Hash.Hex())
+	s3Key := fmt.Sprintf("%s/stateDiff", stateDiff.Hash.Hex())
 	return &DataFile{
 		S3key: s3Key,
 		Data:  data,
