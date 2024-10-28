5d88fc57d1a88708adf5977a7356877b19600a94
Zhang Guyu <zhangguyu6@gmail.com>
2024-10-28 04:18:08 +0000

    fix

--- processor/push.go
+++ processor/push.go
@@ -23,6 +23,7 @@ type PushProcessor struct {
 
 func NewPushProcessor(region string, bucket string, brokers []string, topic string) (*PushProcessor, error) {
 	kafkaReader := util.NewKafkaReader(brokers, topic, "push-processor")
+	fmt.Printf("kafka reader created")
 	kafkaWriter := util.NewKafkaWriterForBlockNotice(brokers, topic)
 	s3Uploader, err := util.NewS3Uploader(region)
 	if err != nil {
