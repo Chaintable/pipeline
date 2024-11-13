721ee80d1d64bfe1d62f8ee8b1e571e40818c0a9
Zhang Guyu <zhangguyu6@gmail.com>
2024-11-13 03:27:52 +0000

    fix

--- types/miner_native_transfer.go
+++ types/miner_native_transfer.go
@@ -1,11 +1,7 @@
 package types
 
-import (
-	"github.com/ethereum/go-ethereum/common"
-)
-
 type MinerNativeRransfer struct {
-	ID     string         `json:"id"`
-	ToAddr common.Address `json:"to_addr"`
-	Value  float64        `json:"value"` //amount / 1e18
+	ID     string  `json:"id"`
+	ToAddr string  `json:"to_addr"`
+	Value  float64 `json:"value"` //amount / 1e18
 }
