2e525f79af8b15e4cf0df19176d515069ddd8531
Zhang Guyu <zhangguyu6@gmail.com>
2024-11-13 03:16:11 +0000

    fix

--- types/withdrawal_native_transfer.go
+++ types/withdrawal_native_transfer.go
@@ -2,14 +2,12 @@ package types
 
 import (
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
 )
 
 type WithdrawalNativeTransfer struct {
-	ID           string         `json:"id"`
-	Idx          int64          `json:"idx"`
-	ValidatorIdx *big.Int       `json:"validator_idx"`
-	ToAddress    common.Address `json:"to_address"`
-	Value        float64        `json:"value"` // amount / 1e18
+	ID           string   `json:"id"`
+	Idx          *big.Int `json:"idx"`
+	ValidatorIdx *big.Int `json:"validator_idx"`
+	ToAddress    string   `json:"to_address"`
+	Value        float64  `json:"value"` // amount / 1e18
 }
