0802143f08fb4336ead23a35fc957a7ec7ab6144
Zhang Guyu <zhangguyu6@gmail.com>
2024-11-13 03:17:58 +0000

    fix

--- types/transaction.go
+++ types/transaction.go
@@ -10,16 +10,16 @@ type Transaction struct {
 	ID   string `json:"id"`
 	From string `json:"from_addr"`
 	// create的时候是合约地址
-	To               string         `json:"to_addr"`
-	Gas              *big.Int       `json:"gas_limit"`
-	GasPrice         *big.Int       `json:"gas_price"`
-	GasUsed          hexutil.Uint64 `json:"gas_used"`
-	Status           bool           `json:"status"`
-	GasFeeCap        *big.Int       `json:"max_fee_per_gas"`
-	GasTipCap        *big.Int       `json:"max_priority_fee_per_gas"`
-	Input            hexutil.Bytes  `json:"input"`
-	Nonce            *big.Int       `json:"nonce"`
-	TransactionIndex int64          `json:"transaction_index"`
-	Value            float64        `json:"value"` // 转成小数  value/1e18
-	Type             int64          `json:"type"`
+	To               string        `json:"to_addr"`
+	Gas              *big.Int      `json:"gas_limit"`
+	GasPrice         *big.Int      `json:"gas_price"`
+	GasUsed          *big.Int      `json:"gas_used"`
+	Status           bool          `json:"status"`
+	GasFeeCap        *big.Int      `json:"max_fee_per_gas"`
+	GasTipCap        *big.Int      `json:"max_priority_fee_per_gas"`
+	Input            hexutil.Bytes `json:"input"`
+	Nonce            *big.Int      `json:"nonce"`
+	TransactionIndex int64         `json:"idx"`
+	Value            float64       `json:"value"` // 转成小数  value/1e18
+	Type             int64         `json:"type"`
 }
