0a308882a9f485588d83837df1f720222136f977
Zhang Guyu <zhangguyu6@gmail.com>
2024-11-27 08:34:21 +0000

    feat: change ReplicaState

--- types/replica_notification.go
+++ types/replica_notification.go
@@ -10,5 +10,5 @@ type ReplicaStateChangeNotification struct {
 type ReplicaState struct {
 	LatestBlockNumber *hexutil.Big `json:"latestBlockNumber"`
 	StateType         uint64       `json:"stateType"` // 1 latest, 2 delay, 3 offline
-	Meta              string       `json:"meta"`      // 副本元数据，ip等，和网关模块进一步约定
+	IP                string       `json:"ip"`        //
 }
