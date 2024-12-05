ae359583da270f19fb2bd8b9e3e9bd84ff8cead2
Zhang Guyu <zhangguyu6@gmail.com>
2024-12-05 09:41:12 +0000

    feat: add node type

--- types/replica_notification.go
+++ types/replica_notification.go
@@ -5,6 +5,7 @@ import "github.com/ethereum/go-ethereum/common/hexutil"
 type ReplicaStateChangeNotification struct {
 	LatestBlockNumber *hexutil.Big   `json:"latestBlockNumber"`
 	ReplicaStates     []ReplicaState `json:"replicaStates"`
+	NodeType          uint64         `json:"nodeType"` // 1 state, 2 archive
 }
 
 type ReplicaState struct {
