a49a68c058934afd64351ca664051295d3346788
Zhang Guyu <zhangguyu6@gmail.com>
2025-08-26 13:51:50 +0000

    fix timer

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -13,17 +13,17 @@ var (
 
 	NodeInfo = metrics.NewRegisteredGaugeInfo("pipeline/node_info", nil)
 
-	BlockProcessTimer = metrics.GetOrRegisterCounter("pipeline/block_process", nil)
+	BlockProcessTimer = metrics.GetOrRegisterTimer("pipeline/block_process", nil)
 
-	BlockTxExecutionTimer = metrics.GetOrRegisterCounter("pipeline/tx_execution", nil)
+	BlockTxExecutionTimer = metrics.GetOrRegisterTimer("pipeline/tx_execution", nil)
 
-	BlockHeaderUploadTimer = metrics.GetOrRegisterCounter("pipeline/block_header_upload", nil)
+	BlockHeaderUploadTimer = metrics.GetOrRegisterTimer("pipeline/block_header_upload", nil)
 
-	StateDiffUploadTimer = metrics.GetOrRegisterCounter("pipeline/state_diff_upload", nil)
+	StateDiffUploadTimer = metrics.GetOrRegisterTimer("pipeline/state_diff_upload", nil)
 
-	BlockFileUploadTimer = metrics.GetOrRegisterCounter("pipeline/block_file_upload", nil)
+	BlockFileUploadTimer = metrics.GetOrRegisterTimer("pipeline/block_file_upload", nil)
 
-	BlockFileValidationTimer = metrics.GetOrRegisterCounter("pipeline/block_file_validation", nil)
+	BlockFileValidationTimer = metrics.GetOrRegisterTimer("pipeline/block_file_validation", nil)
 
-	BlockPushTimer = metrics.GetOrRegisterCounter("pipeline/block_push", nil)
+	BlockPushTimer = metrics.GetOrRegisterTimer("pipeline/block_push", nil)
 )
