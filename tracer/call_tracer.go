95986bd1183ad886586f2b96d389da1339cc55ca
Zhang Guyu <zhangguyu6@gmail.com>
2025-09-18 10:11:42 +0000

    fix: correct error trace logging in callTracer

--- tracer/call_tracer.go
+++ tracer/call_tracer.go
@@ -352,7 +352,7 @@ func (t *callTracer) addTraceAndLog(cf *callFrame, traceAddress []int64) {
 	}
 	for i := range cf.Calls {
 		if cf.failed() || cf.ParentFailed {
-			t.BlockFile.ErrorTraces = append(t.BlockFile.Traces, t.ToTrace(&cf.Calls[i], childTraceAddress(traceAddress, int64(i))))
+			t.BlockFile.ErrorTraces = append(t.BlockFile.ErrorTraces, t.ToTrace(&cf.Calls[i], childTraceAddress(traceAddress, int64(i))))
 		} else {
 			t.BlockFile.Traces = append(t.BlockFile.Traces, t.ToTrace(&cf.Calls[i], childTraceAddress(traceAddress, int64(i))))
 		}
